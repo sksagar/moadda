eab6b6529b4c7db6ecf8b996aec9232571a07017
sagar <sahoosagar1999@gmail.com>
2020-11-17 12:02:59 +0530

    avatar changed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,13 @@ class PostsController < ApplicationController
 
   def find_post
     @post = Post.includes(comments: :user).find(params[:id])
-    render json: @post.as_json(include: { comments: { include: :user},user: {except: :avatar}  })
+    data = @post.as_json(include: { comments: { include: :user}})
+    if @post.user.avatar.attached? 
+      data[:user] = {username: @post.user.username, avatar_url: Rails.application.routes.url_helpers.rails_blob_path(@post.user.avatar, only_path: true)}
+    else
+      data[:user] = {username: @post.user.username}
+    end
+    render json: data
   end
 
   private
